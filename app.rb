3feb3fdbfc45a19004816ee7a29d7c4023fa889f
dobri-dobrev <dmd2169@columbia.edu>
2014-03-13 22:07:59 -0400

    blah

--- app.rb
+++ app.rb
@@ -2,12 +2,12 @@ require 'sinatra'
 require 'json'
 require 'pony'
 
-configure :production, :development do
-    enable :logging
-end
-before {
-  env['rack.logger'] = Logger.new "#{settings.root}/log/#{settings.environment}.log"
-}
+# configure :production, :development do
+#     enable :logging
+# end
+# before {
+#   env['rack.logger'] = Logger.new "#{settings.root}/log/#{settings.environment}.log"
+# }
 
 get '/' do
 	erb :index
@@ -22,8 +22,8 @@ get '/campaign' do
 end
 
 post '/send_email' do
-	puts params[:email]
-	logger.info params[:email]
+	# puts params[:email]
+	# logger.info params[:email]
 	content_type :json
 	{ :key1 => 'value1', :key2 => 'value2' }.to_json
 end
